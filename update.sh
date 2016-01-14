d38c01da9493c38588afbd173f39e9e69dcb48fd
Patrick Boos <patrickboos85@gmail.com>
2016-01-14 14:06:02 +0100

    Make it work on mac os x

--- update.sh
+++ update.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -eo pipefail
 
-cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+if [ "$(uname)" == "Darwin" ]; then
+	cd "$(dirname $0)"
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+	cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+fi
 
 versions=( "$@" )
 if [ ${#versions[@]} -eq 0 ]; then
@@ -14,14 +18,18 @@ travisEnv=
 for version in "${versions[@]}"; do
 	dist="${version//./}"
 	packagesUrl="http://www.apache.org/dist/cassandra/debian/dists/${dist}x/main/binary-amd64/Packages.gz"
-	fullVersion="$(curl -fsSL "$packagesUrl" | gunzip | grep -m1 -A10 "^Package: cassandra\$" | grep -m1 '^Version: ' | cut -d' ' -f2)"
-	
+	if [ "$(uname)" == "Darwin" ]; then
+		fullVersion="$(curl -fsSL "$packagesUrl" | gunzip | grep -m1 '^Version: ' | cut -d' ' -f2)"
+	elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+		fullVersion="$(curl -fsSL "$packagesUrl" | gunzip | grep -m1 -A10 "^Package: cassandra\$" | grep -m1 '^Version: ' | cut -d' ' -f2)"
+	fi
+
 	(
 		set -x
 		cp docker-entrypoint.sh "$version/"
 		sed 's/%%CASSANDRA_DIST%%/'$dist'/g; s/%%CASSANDRA_VERSION%%/'$fullVersion'/g' Dockerfile.template > "$version/Dockerfile"
 	)
-	
+
 	travisEnv='\n  - VERSION='"$version$travisEnv"
 done
 
